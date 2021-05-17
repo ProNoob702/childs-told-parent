00fcd15915b50f449f6c4eac4119f1cd3a930b6a
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2021-05-17 17:43:23 +0100

    upload multi is ok

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,115 +1,156 @@
-import React, { useState, useEffect, useRef } from 'react'
+// [Upload EXAMPLE]
+import React, { useState } from 'react'
+import axios from 'axios'
 
-import { IChildHeights, useChildsDidUpdate } from 'childs-told-parent'
-import 'childs-told-parent/dist/index.css'
-import { useDispatch } from 'react-redux'
-import { appActions } from './redux-store/slice'
-
-const rawChildsList: string[] = ['child1', 'child2', 'child3']
-
-const sumHeights = (childsHeights: IChildHeights): number => {
-  const keys = Object.keys(childsHeights)
-  let sum = 0
-  keys.forEach((key) => {
-    sum += childsHeights[key]
-  })
-  return sum
-}
+const backendUrl = `https://localhost:44384/Files`
 
 const App: React.FC<{}> = () => {
-  const [childsList, setChildsList] = useState(rawChildsList)
-  const [height, setHeight] = useState(0)
-  const onAllChildsReady = (childsHeights: IChildHeights) => {
-    console.log('Parent: ', childsHeights)
-    const newHeight = sumHeights(childsHeights)
-    setHeight(newHeight + 20)
+  const [files, setFiles] = useState<FileList>()
+  const submit = (e: React.FormEvent<HTMLFormElement>) => {
+    if (files && files.length) {
+      e.preventDefault()
+      const formData = new FormData()
+      for (let i = 0; i < files.length; i++) {
+        formData.append('Files', files[i])
+      }
+      const config = {
+        headers: {
+          'content-type': 'multipart/form-data'
+        }
+      }
+      return axios.post(backendUrl, formData, config)
+    }
+    return null
   }
 
-  const [] = useChildsDidUpdate(childsList.length, onAllChildsReady)
-  const handleInpuBlur = (e: React.FocusEvent<any>) => {
-    const newValue = e.currentTarget.value
-    const newList = [...childsList, newValue]
-    setChildsList(newList)
+  const handleSetFile = (e: React.FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget && e!.currentTarget?.files?.length) {
+      setFiles(e!.currentTarget!.files)
+    }
   }
+
   return (
-    <>
-      <div
-        className='zouza'
-        style={{
-          minHeight: height,
-          background: 'blue',
-          display: 'flex',
-          flexDirection: 'column'
-        }}
-      >
-        {childsList.map((x) => (
-          <ChildHasChanges key={x} />
-        ))}
-        Parent Height : {height} (Chils Heights + 20)
-      </div>
-      <div className='editingZone'>
-        <p> Add New Child</p>
-        <input type='text' onBlur={handleInpuBlur} />
-      </div>
-    </>
+    <form onSubmit={(e) => submit(e)}>
+      <h1>File Upload</h1>
+      <input type='file' multiple onChange={(e) => handleSetFile(e)} />
+      <button type='submit'>Upload</button>
+    </form>
   )
 }
 
-interface IChildProps {
-  id: string
-}
+// [SCHEDULER EXAMPLE]
+// import React, { useState, useEffect, useRef } from 'react'
 
-const ChildHasChanges: React.FC<IChildProps> = ({ id }) => {
-  const dispatch = useDispatch()
-  const [counter, setCounter] = useState(50)
-  const childRef = React.useRef<HTMLDivElement | any>()
-  useDidUpdate(() => {
-    debugger
-    if (childRef.current) {
-      dispatch(
-        appActions.setChildHeight({
-          key: id,
-          newHeight: childRef.current!.offsetHeight
-        })
-      )
-    }
-  }, [counter])
+// import { IChildHeights, useChildsDidUpdate } from 'childs-told-parent'
+// import 'childs-told-parent/dist/index.css'
+// import { useDispatch } from 'react-redux'
+// import { appActions } from './redux-store/slice'
 
-  useEffect(() => {
-    debugger
-    if (childRef.current) {
-      dispatch(
-        appActions.setChildHeight({
-          key: id,
-          newHeight: childRef.current!.offsetHeight
-        })
-      )
-    }
-  }, [])
+// const rawChildsList: string[] = ['child1', 'child2', 'child3']
 
-  const updateCount = () => {
-    setCounter((x) => x + 1)
-  }
-  return (
-    <div
-      style={{ height: counter, background: 'red', display: 'flex' }}
-      ref={childRef}
-    >
-      <p>Height : {counter}</p>
-      <button onClick={updateCount}>Add Height</button>
-    </div>
-  )
-}
+// const sumHeights = (childsHeights: IChildHeights): number => {
+//   const keys = Object.keys(childsHeights)
+//   let sum = 0
+//   keys.forEach((key) => {
+//     sum += childsHeights[key]
+//   })
+//   return sum
+// }
 
-function useDidUpdate(callback: () => void, deps: any[]) {
-  const hasMount = useRef(false)
-  useEffect(() => {
-    if (hasMount.current) {
-      callback()
-    } else {
-      hasMount.current = true
-    }
-  }, deps)
-}
+// const App: React.FC<{}> = () => {
+//   const [childsList, setChildsList] = useState(rawChildsList)
+//   const [height, setHeight] = useState(0)
+//   const onAllChildsReady = (childsHeights: IChildHeights) => {
+//     console.log('Parent: ', childsHeights)
+//     const newHeight = sumHeights(childsHeights)
+//     setHeight(newHeight + 20)
+//   }
+
+//   const [] = useChildsDidUpdate(childsList.length, onAllChildsReady)
+//   const handleInpuBlur = (e: React.FocusEvent<any>) => {
+//     const newValue = e.currentTarget.value
+//     const newList = [...childsList, newValue]
+//     setChildsList(newList)
+//   }
+//   return (
+//     <>
+//       <div
+//         className='zouza'
+//         style={{
+//           minHeight: height,
+//           background: 'blue',
+//           display: 'flex',
+//           flexDirection: 'column'
+//         }}
+//       >
+//         {childsList.map((x) => (
+//           <ChildHasChanges key={x} id={x} />
+//         ))}
+//         Parent Height : {height} (Chils Heights + 20)
+//       </div>
+//       <div className='editingZone'>
+//         <p> Add New Child</p>
+//         <input type='text' onBlur={handleInpuBlur} />
+//       </div>
+//     </>
+//   )
+// }
+
+// interface IChildProps {
+//   id: string
+// }
+
+// const ChildHasChanges: React.FC<IChildProps> = ({ id }) => {
+//   const dispatch = useDispatch()
+//   const [counter, setCounter] = useState(50)
+//   const childRef = React.useRef<HTMLDivElement | any>()
+//   useDidUpdate(() => {
+//     debugger
+//     if (childRef.current) {
+//       dispatch(
+//         appActions.setChildHeight({
+//           key: id,
+//           newHeight: childRef.current!.offsetHeight
+//         })
+//       )
+//     }
+//   }, [counter])
+
+//   useEffect(() => {
+//     debugger
+//     if (childRef.current) {
+//       dispatch(
+//         appActions.setChildHeight({
+//           key: id,
+//           newHeight: childRef.current!.offsetHeight
+//         })
+//       )
+//     }
+//   }, [])
+
+//   const updateCount = () => {
+//     setCounter((x) => x + 1)
+//   }
+//   return (
+//     <div
+//       style={{ height: counter, background: 'red', display: 'flex' }}
+//       ref={childRef}
+//     >
+//       <p>Height : {counter}</p>
+//       <button onClick={updateCount}>Add Height</button>
+//     </div>
+//   )
+// }
+
+// function useDidUpdate(callback: () => void, deps: any[]) {
+//   const hasMount = useRef(false)
+//   useEffect(() => {
+//     if (hasMount.current) {
+//       callback()
+//     } else {
+//       hasMount.current = true
+//     }
+//   }, deps)
+// }
 
 export default App
